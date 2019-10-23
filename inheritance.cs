730e9061038b99aafb72a60d90f1bb147e8dc89a
ally2211 <amendieta.va@gmail.com>
2019-10-22 20:52:11 -0400

    inheritance.cs is added for first time

---
+++ inheritance.cs
@@ -0,0 +1,45 @@
+
+using System;
+
+public class A
+{
+    public string Name;
+    public void GetName()
+    {
+        Console.WriteLine("Name: {0}", Name);
+    }
+}
+public class B : A
+{
+    public string Location;
+    public void GetLocation()
+    {
+        Console.WriteLine("Location: {0}", Location);
+    }
+}
+
+public class C : B
+{
+    public int Age;
+    public void GetAge()
+    {
+        Console.WriteLine("Age: {0}", Age);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        C c = new C();
+        c.Name = "Allyson usa";
+        c.Location = "United States";
+        c.Age = 32;
+        c.GetName();
+        c.GetLocation();
+        c.GetAge();
+        Console.WriteLine("Press any key to exit World");
+        Console.ReadLine();
+    }
+
+}
