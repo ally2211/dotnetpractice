de256023fb509da5af08d6d61b502cfc37a49f4f
ally2211 <amendieta.va@gmail.com>
2019-10-26 18:25:15 -0400

    "updated to change response text in array"

--- ToDoAPI/WeatherForecast.cs
+++ ToDoAPI/WeatherForecast.cs
@@ -4,11 +4,11 @@ namespace ToDoAPI
 {
     public class WeatherForecast
     {
-        public DateTime Date { get; set; }
+        public DateTime DateAndTime { get; set; }
 
-        public int TemperatureC { get; set; }
+        public int TemperatureInCelsius { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureInFarenheit => 32 + (int)(TemperatureInCelsius / 0.5556);
 
         public string Summary { get; set; }
     }
